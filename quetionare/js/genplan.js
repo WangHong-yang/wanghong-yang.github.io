0c6e1c1e9d255d13d4f68ca116412576d33482cd
wanghong-yang <549398718@qq.com>
2015-11-27 22:14:19 +0800

    autoinhome/quetionare
    test edition, all rights reserved

---
+++ quetionare/js/genplan.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var storage = window.localStorage; // local storage
+    var price = 0;
+
+    
+    
+    if (storage.classic_score==undefined) {
+        storage.classic_score = 0;
+    }
+}
