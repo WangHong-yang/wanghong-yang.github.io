1e53adea034ff06ae4f9838722051f5066a47c82
Hongyang Wang <hongyang.wang@sv.cmu.edu>
2017-02-05 12:29:14 -0800

    Add new Angular UI components

--- vimeoOA/js/app.js
+++ vimeoOA/js/app.js
@@ -1,38 +1,73 @@
 (function() {
     var app = angular.module('vimeoCC', ['vimeoModules']);
     
-    app.controller('VimeoController', function(){
+    app.controller('VimeoCarouselController', function(){
 		this.slides = slideList;
 	});
+    app.controller('VimeoTextImageController', function(){
+        this.textImages = textImageList;
+    });
+    // app.controller('VimeoMonsoonController', function(){
+    //     this.monsoon = textImageList[0];
+    // });
+    // app.controller('VimeoBeamsController', function(){
+    //     this.beams = textImageList[1];
+    // });
+    // app.controller('VimeoMoveController', function(){
+    //     this.move = textImageList[2];
+    // });
 
 	var slideList = [
-		{
-            imageURL: './assets/NICHTS.jpg',
-            h2: 'Nichts passiert / A Decent Man',
-            p: 'A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandaries.',
-            layerColor: [71, 109, 122], // RGB
-            btnColor: '#1fb8e7'
-        },
         {
-            imageURL: './assets/hunt.jpg',
+            imageURL: './img/hunt.jpg',
             h2: 'Hunt for the Wilderpeople',
             p: 'Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the Director of What We Do in The Shadows.',
             layerColor: [67, 99, 123], // RGB
             btnColor: '#3598D4'
         },
         {
-            imageURL: './assets/vice.jpg',
+            imageURL: './img/vice.jpg',
             h2: 'Vice Versa',
             p: 'Come along with the good company crew as they travel throughout the US, Japan, BC and Quebec to showcase skiing in the best way possible.',
+            layerColor: [95, 93, 70], // RGB
+            btnColor: '#acab58'
+        },
+        {
+            imageURL: './img/NICHTS.jpg',
+            h2: 'Nichts passiert / A Decent Man',
+            p: 'A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandaries.',
             layerColor: [71, 109, 122], // RGB
             btnColor: '#1fb8e7'
         },
         {
-            imageURL: './assets/fullmoon.jpg',
+            imageURL: './img/fullmoon.jpg',
             h2: 'Full Moon',
             p: 'Be inspired by women who push boundaries. Full Moon showcases legends and current icons of this ever evolving lifestyle sport.',
-            layerColor: [71, 109, 122], // RGB
-            btnColor: '#1fb8e7'
+            layerColor: [80, 80, 80], // RGB
+            btnColor: '#9a989a'
         }
     ];
+    var textImageList = [
+        {
+            imageURL: 'https://i.vimeocdn.com/video/595198868_505x160.jpg',
+            h2: 'MONSOON III',
+            p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta dolor varius felis vehicula iaculis. Integer consequat bibendum ex nec pulvinar. In eu bibendum dui. Nulla sed felis convallis, convallis dui ornare, vestibulum arcu. Pellentesque sollicitudin ac ligula eu blandit. Maecenas id tellus nulla.',
+            ifColumnReverse: true,
+            ifRowReverse: false
+        }, 
+        {
+            imageURL: 'https://i.vimeocdn.com/video/589972810_530x315.jpg',
+            h2: 'BEAMS',
+            p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta dolor varius felis vehicula iaculis. Integer consequat bibendum ex nec pulvinar. In eu bibendum dui. Nulla sed felis convallis, convallis dui ornare, vestibulum arcu. Pellentesque sollicitudin ac ligula eu blandit. Maecenas id tellus nulla.',
+            ifColumnReverse: true,
+            ifRowReverse: true
+        }, 
+        {
+            imageURL: 'https://i.vimeocdn.com/video/590587169_530x315.jpg',
+            h2: 'MOVE 2',
+            p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta dolor varius felis vehicula iaculis. Integer consequat bibendum ex nec pulvinar. In eu bibendum dui. Nulla sed felis convallis, convallis dui ornare, vestibulum arcu. Pellentesque sollicitudin ac ligula eu blandit. Maecenas id tellus nulla.',
+            ifColumnReverse: true,
+            ifRowReverse: false
+        }
+    ]
 }())
--- vimeoOA/js/appModule.js
+++ vimeoOA/js/appModule.js
@@ -7,6 +7,35 @@
 			templateUrl: 'vimeo-slides.html'
 		};
 	});
+	app.directive('vimeoCarousel', function(){
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'vimeo-carousel.html'
+		};
+	});
+	app.directive('vimeoMonsoon', function(){
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'vimeo-monsoon.html'
+		};
+	});
+	app.directive('vimeoBeamsMove', function(){
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'vimeo-beams-move.html'
+		};
+	});
+	app.directive('vimeoTextImage', function(){
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'vimeo-text-image.html'
+		};
+	});
+
 	app.directive('slidesReady', function(){
 		return {
 			priority: -1000, // a low number so this directive loads after all other directives have loaded. 
@@ -14,13 +43,13 @@
             link: function($scope, $element, $attributes) {
                 
 				$element.ready(function(){
-					console.log(" -- Element ready!");
 					var swiper = new Swiper('.swiper-container', {
 						nextButton: '.swiper-button-next',
 						prevButton: '.swiper-button-prev',
 						slidesPerView: 1,
 						speed: 400,
 						spaceBetween: 0,
+						simulateTouch: false,
 						loop: true,
 						autoplay: 2500,
         				autoplayDisableOnInteraction: true
@@ -30,10 +59,4 @@
             }
 		}
 	});
-	// app.directive('jobSearch', function(){
-	// 	return {
-	// 		restrict: 'E',
-	// 		templateUrl: 'job-search.html'
-	// 	};
-	// });
 })();
